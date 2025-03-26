a835aaffa65b75fa37efd5aa126d9184f2d0127d
Thalys <thalysfarias11@gmail.com>
2025-03-26 17:07:30 -0300

    chore: corrigindo identação

--- internal/models/campus.go
+++ internal/models/campus.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Campus struct {
-	ID          string `json:"id"`
-	Name        string `json:"name" validate:"required"`
-	Description string `json:"description"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name" validate:"required"`
+	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"-"`
 	UpdatedAt   time.Time `json:"-"`
-	UserOwnerID string `json:"-"`
+	UserOwnerID string    `json:"-"`
 }
